436351b9b7663179e3aaa97485e13d602071555d
praveen palanivel <praveenpalanivel@gmail.com>
2013-11-28 17:47:41 +0000

    28112013_1117

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,31 @@
+<?php
+session_start();
+if(!(isset($_SESSION['id']) && isset($_SESSION['usertype'])))
+{
+    header('location:../index.php');
+}
+if(! $_SESSION['usertype'] == 0)
+{
+    header('location:../index.php');    
+}
+?>
 <html>
   <head>
      <title>Admin Home</title>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+     <script type="text/javascript" src="../js/jquery.min.js"></script>
+     <script type="text/javascript" src="../js/adminscript.js"></script>
+     <link rel="stylesheet" type="text/css" href="admin.css">
   </head>
   <body>
-     <a href="addassocation.php">Add Assocation</a>
+  <div>
+     <h4>Welcome <?=$_SESSION['username']?></h4>
+     <span class="logout">
+       <a href="logout.php">Logut </a>
+     </span>
+  </div>
+  <div>
+    <a href="addassocation.php">Add Assocation</a>
+  </div>
   </body>
 </html>
--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$database_server = mysql_connect('223.30.82.222','root','') or die('Database server not connected');
-$database = mysql_selectdb('devp',$database_server)  or die('Database not connected');
+$database_server = mysql_connect('198.58.84.196','ppraveen_aurum','aurum') or die('Database server not connected');
+$database = mysql_selectdb('ppraveen_aurum',$database_server)  or die('Database not connected');
 $usertype = array('0'=>'admin','1'=>'assocation','2'=>'members');
