f83fb82e4896436e260ba606211f3306c217e34a
praveen palanivel <praveenpalanivel@gmail.com>
2013-11-28 05:12:23 +0000

    Codenvy: Factory URL - 2013-11-28T10:42+05:30

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$database_server = mysql_connect('sql3.freesqldatabase.com','sql321152','bX8%zZ8%') or die('Database server not connected');
-$database = mysql_selectdb('sql321152',$database_server)  or die('Database not connected');
+$database_server = mysql_connect('223.30.82.222','root','') or die('Database server not connected');
+$database = mysql_selectdb('devp',$database_server)  or die('Database not connected');
 $usertype = array('0'=>'admin','1'=>'assocation','2'=>'members');
