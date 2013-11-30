dce4ed7228ca4b97f2e776422a0a86280eccc752
praveen palanivel <praveenpalanivel@gmail.com>
2013-11-30 16:48:56 +0000

    association added 30112013

---
+++ admin/association_option.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+     <title>Options</title>
+  </head>
+  <body>
+  <div class="association_option_content">
+    <h3 align="center">Association name</h3>
+    <div>Daily Sms Count : 2 &nbsp;&nbsp;<span id='addsmscount'>+</span>&nbsp;&nbsp;<span id="minussmscount">-</span></div>
+  </div>  
+  </body>
+</html>
